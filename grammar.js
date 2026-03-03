7cfb6ce38f0c3fbe32925f6ae205131e004d91d8
twestura <twestura@gmail.com>
2026-03-03 06:25:15 -0500

    Basic grammar for defines and constants.

--- grammar.js
+++ grammar.js
@@ -10,8 +10,16 @@
 export default grammar({
   name: "aoe2_rms",
 
+  // TODO use only ascii whitespace, check whether vertical tab and form feed count
+  extras: ($) => [/[ \t\r\n]/],
+  // extras: ($) => [/[ \t\r\n\v\f]/],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    source_file: ($) => repeat($._definition),
+    _definition: ($) => choice($.define_statement, $.const_statement),
+    define_statement: ($) => seq("#define", $.identifier),
+    const_statement: ($) => seq("#const", $.identifier, $.integer),
+    identifier: ($) => /\S+/,
+    integer: ($) => /[0-9]+/,
+  },
 });
